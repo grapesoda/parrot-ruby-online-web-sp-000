c5ea6d9e3e3bd89300512e70a95fc302171f88f5
Tres Tarwater <trest03@gmail.com>
2020-10-21 19:33:58 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def greeting(name = "Ruby programmer")
-  puts "Hello, #{name}"
+def parrot(phrase = "Squawk!")
+  puts "#{name}"
 end
